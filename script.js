60a54ff05b9981dfdf7269902f3c2c1339234d1c
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2024-12-17 02:10:11 -0800

    Final touches

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function createGrid (num) {
     body.removeChild(document.querySelector(".container"));
     const container = document.createElement("div");
     container.classList.add("container");
-    body.appendChild(container);
+    body.insertBefore(container, button);
     
-    const size = "width: " + (800 / num) + "px; " + "height: " + (800 / num) + "px;";
+    const size = "width: " + (700 / num) + "px; " + "height: " + (700 / num) + "px;";
     for (let i = 0; i < num * num; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
@@ -13,24 +13,25 @@ function createGrid (num) {
         container.appendChild(square);
     }
     const squares = document.querySelectorAll(".square");
-    addHovering(squares);
+    squares.forEach(addHovering);
 }
 
-function addHovering(squares) {
-    squares.forEach((square) => {
-        square.addEventListener("mouseover", () => {
-            const r = Math.floor(Math.random() * 256); 
-            const g = Math.floor(Math.random() * 256);
-            const b = Math.floor(Math.random() * 256);
-            square.style.backgroundColor = `rgb(${r}, ${g}, ${b}`;
-        });
+function addHovering(square) {
+    square.addEventListener("mouseover", () => {
+        const r = Math.floor(Math.random() * 256); 
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        square.style.backgroundColor = `rgb(${r}, ${g}, ${b}`;
     });
 }
 
 const body = document.querySelector("body");
-createGrid(16);
+
+const title = document.querySelector("h1");
+addHovering(title);
 
 const button = document.querySelector(".grid-size");
+addHovering(button);
 button.addEventListener("click", () => {
     const num = prompt("Choose a size between 1 - 100");
     if (!(0 < num && num <= 100)) {
@@ -39,3 +40,5 @@ button.addEventListener("click", () => {
     }
     createGrid(num);
 });
+
+createGrid(16);
